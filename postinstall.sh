0a4f9f45a0c8650f33734b78d1da5a09dd129779
nemorino <nemorino@sdf.org>
2026-08-29 14:37:07 -0400

    fix bug

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,7 @@
 
 systemctl enable xdm-archlinux
 for user in `ls -1 /home`; do
-		xsession = "/home/${user}/.xsession"
+		xsession="/home/${user}/.xsession"
 		echo "awesome" >> $xsession
 		chown ${user}:${user} $xsession
 		chmod +x $xsession
