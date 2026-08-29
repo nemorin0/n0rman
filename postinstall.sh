8b9eff844b013d56a6d86fe21d71bcb82ccbe07f
nemorino <nemorino@sdf.org>
2026-08-29 14:19:28 -0400

    add postinstall

---
+++ postinstall.sh
@@ -0,0 +1,9 @@
+#/usr/bin/bash
+
+systemctl enable xdm-archlinux
+for user in `ls -1 /home`; do
+		xsession = "/home/${user}/.xsession"
+		echo "awesome" >> $xsession
+		chown ${user}:${user} $xsession
+		chmod +x $xsession
+done
